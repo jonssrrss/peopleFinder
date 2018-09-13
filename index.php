a9086e0dee190b8d6bf47d8177ae5c6377a04aba
jonssrrss <31797548+jonssrrss@users.noreply.github.com>
2018-09-13 15:52:48 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="styles/style.css" />
+	<meta charset="UTF-8">
+	<title>Hello</title>
+</head>
+<body>
+	<header>
+		<div class="headerContainer">
+			<div class="logo"><a href="/"><img src="images/logo2.svg"><h1>PeopleFinder</h1></a></div>
+			<div class="cache"><img src="images/purse.svg"><h2>40 ₽</h2></div>
+		</div>
+	</header>
+	<nav>
+		<ul>
+			<li><div class="navi" id="naviSet"></div><a href="">Настройки</a></li>
+			<li><div class="navi" id="naviMes"></div><a href="">Сообщения</a></li>
+			<li><div class="navi" id="naviPeople"></div><a href="">Люди</a></li>
+			<li><a href="">4 пункт</a></li>
+			<li><a href="">5 пункт</a></li>
+		</ul>
+	</nav>
+	<div class="wrapper">
+		<div class="tabs">
+			<ul class="tab">
+				<li class="active"><a href="/">Все люди</a></li>
+				<li><a href="">Чаты</a></li>
+				<li><a href="">Лайки</a></li>
+				<li><a href="">Объявления</li>
+			</ul>
+		</div>
+		<div class="peoples">
+			<ul class="people">
+				<li>
+					<a href="id1">
+						<div class="peCam"><img src="images/camera.svg"><div class="colPeImg">12</div></div>
+						<div class="heart"><img src="images/heart.png"></div>
+						<div class="peImg"><img src="http://jxon.ru/photos/17.jpg"></div>
+						<div class="peFoot">
+							<div class="pf1">
+								<div class="peName">Евгений, 23</div>
+								<div class="peCity">Уфа</div>
+							</div>
+							<div class="pf2">
+								<div class="o"></div>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="id1">
+						<div class="peCam"><img src="images/camera.svg"><div class="colPeImg">12</div></div>
+						<div class="heart"><img src="images/heart.png"></div>
+						<div class="peImg"><img src="http://jxon.ru/photos/6.jpg"></div>
+						<div class="peFoot">
+							<div class="pf1">
+								<div class="peName">Евгений, 23</div>
+								<div class="peCity">Уфа</div>
+							</div>
+							<div class="pf2">
+								<div class="o"></div>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="id1">
+						<div class="peCam"><img src="images/camera.svg"><div class="colPeImg">12</div></div>
+						<div class="heart"><img src="images/heart.png"></div>
+						<div class="peImg"><img src="http://jxon.ru/photos/23.jpg"></div>
+						<div class="peFoot">
+							<div class="pf1">
+								<div class="peName">Евгений, 23</div>
+								<div class="peCity">Уфа</div>
+							</div>
+							<div class="pf2">
+								<div class="o"></div>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="id1">
+						<div class="peCam"><img src="images/camera.svg"><div class="colPeImg">12</div></div>
+						<div class="heart"><img src="images/heart.png"></div>
+						<div class="peImg"><img src="http://jxon.ru/photos/10.jpg"></div>
+						<div class="peFoot">
+							<div class="pf1">
+								<div class="peName">Евгений, 23</div>
+								<div class="peCity">Уфа</div>
+							</div>
+							<div class="pf2">
+								<div class="o"></div>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="id1">
+						<div class="peCam"><img src="images/camera.svg"><div class="colPeImg">12</div></div>
+						<div class="heart"><img src="images/heart.png"></div>
+						<div class="peImg"><img src="http://jxon.ru/photos/25.jpg"></div>
+						<div class="peFoot">
+							<div class="pf1">
+								<div class="peName">Евгений, 23</div>
+								<div class="peCity">Уфа</div>
+							</div>
+							<div class="pf2">
+								<div class="o"></div>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="id1">
+						<div class="peCam"><img src="images/camera.svg"><div class="colPeImg">12</div></div>
+						<div class="heart"><img src="images/heart.png"></div>
+						<div class="peImg"><img src="http://jxon.ru/photos/5.jpg"></div>
+						<div class="peFoot">
+							<div class="pf1">
+								<div class="peName">Евгений, 23</div>
+								<div class="peCity">Уфа</div>
+							</div>
+							<div class="pf2">
+								<div class="o"></div>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="id1">
+						<div class="peCam"><img src="images/camera.svg"><div class="colPeImg">12</div></div>
+						<div class="heart"><img src="images/heart.png"></div>
+						<div class="peImg"><img src="http://jxon.ru/photos/21.jpg"></div>
+						<div class="peFoot">
+							<div class="pf1">
+								<div class="peName">Евгений, 23</div>
+								<div class="peCity">Уфа</div>
+							</div>
+							<div class="pf2">
+								<div class="o"></div>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="id1">
+						<div class="peCam"><img src="images/camera.svg"><div class="colPeImg">12</div></div>
+						<div class="heart"><img src="images/heart.png"></div>
+						<div class="peImg"><img src="http://jxon.ru/photos/27.jpg"></div>
+						<div class="peFoot">
+							<div class="pf1">
+								<div class="peName">Евгений, 23</div>
+								<div class="peCity">Уфа</div>
+							</div>
+							<div class="pf2">
+								<div class="o"></div>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="id1">
+						<div class="peCam"><img src="images/camera.svg"><div class="colPeImg">12</div></div>
+						<div class="heart"><img src="images/heart.png"></div>
+						<div class="peImg"><img src="http://jxon.ru/photos/5.jpg"></div>
+						<div class="peFoot">
+							<div class="pf1">
+								<div class="peName">Евгений, 23</div>
+								<div class="peCity">Уфа</div>
+							</div>
+							<div class="pf2">
+								<div class="o"></div>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="id1">
+						<div class="peCam"><img src="images/camera.svg"><div class="colPeImg">12</div></div>
+						<div class="heart"><img src="images/heart.png"></div>
+						<div class="peImg"><img src="http://jxon.ru/photos/5.jpg"></div>
+						<div class="peFoot">
+							<div class="pf1">
+								<div class="peName">Евгений, 23</div>
+								<div class="peCity">Уфа</div>
+							</div>
+							<div class="pf2">
+								<div class="o"></div>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="id1">
+						<div class="peCam"><img src="images/camera.svg"><div class="colPeImg">12</div></div>
+						<div class="heart"><img src="images/heart.png"></div>
+						<div class="peImg"><img src="http://jxon.ru/photos/5.jpg"></div>
+						<div class="peFoot">
+							<div class="pf1">
+								<div class="peName">Евгений, 23</div>
+								<div class="peCity">Уфа</div>
+							</div>
+							<div class="pf2">
+								<div class="o"></div>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="id1">
+						<div class="peCam"><img src="images/camera.svg"><div class="colPeImg">12</div></div>
+						<div class="heart"><img src="images/heart.png"></div>
+						<div class="peImg"><img src="http://jxon.ru/photos/3.jpg"></div>
+						<div class="peFoot">
+							<div class="pf1">
+								<div class="peName">Евгений, 23</div>
+								<div class="peCity">Уфа</div>
+							</div>
+							<div class="pf2">
+								<div class="o"></div>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="id1">
+						<div class="peCam"><img src="images/camera.svg"><div class="colPeImg">12</div></div>
+						<div class="heart"><img src="images/heart.png"></div>
+						<div class="peImg"><img src="http://jxon.ru/photos/2.jpg"></div>
+						<div class="peFoot">
+							<div class="pf1">
+								<div class="peName">Евгений, 23</div>
+								<div class="peCity">Уфа</div>
+							</div>
+							<div class="pf2">
+								<div class="o"></div>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="id1">
+						<div class="peCam"><img src="images/camera.svg"><div class="colPeImg">12</div></div>
+						<div class="heart"><img src="images/heart.png"></div>
+						<div class="peImg"><img src="http://jxon.ru/photos/1.jpg"></div>
+						<div class="peFoot">
+							<div class="pf1">
+								<div class="peName">Евгений, 23</div>
+								<div class="peCity">Уфа</div>
+							</div>
+							<div class="pf2">
+								<div class="o"></div>
+							</div>
+						</div>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<footer>
+		<div class="line"></div>
+		<div class="footContain">
+			<div class="footNav">
+				<div class="fn"><a href="">О проекте</a></div>
+				<div class="fn"><a href="">Поддержка</a></div>
+				<div class="fn"><a href="">Блог</a></div>
+				<div class="fn"><a href="">Реклама</a></div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ profile.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="styles/style.css" />
+	<meta charset="UTF-8">
+	<title>Hello</title>
+</head>
+<body>
+	<header>
+		<div class="headerContainer">
+			<div class="logo"><a href="/"><img src="images/logo.svg"><h1>PeopleFinder</h1></a></div>
+		</div>
+	</header>
+	<nav>
+		<ul>
+			<li><div class="navi" id="naviSet"></div><a href="">Настройки</a></li>
+			<li><div class="navi" id="naviMes"></div><a href="">Сообщения</a></li>
+			<li><div class="navi" id="naviPeople"></div><a href="">Люди</a></li>
+			<li><a href="">4 пункт</a></li>
+			<li><a href="">5 пункт</a></li>
+		</ul>
+	</nav>
+	<div class="wrapper">
+		<div class="tabs">
+			<ul class="tab">
+				<li><a href="">Чаты</a></li>
+				<li><a href="">Лайки</a></li>
+				<li><a href="/">Все люди</a></li>
+			</ul>
+		</div>
+		<div class="profile">
+
+
+
+			<div class="headProf">
+				<div class="fonProf"><img src="http://jxon.ru/photos/1.jpg"></div>
+			</div>
+			<div class="infoProf">
+				<div class="b">Зовут</div>
+				<div class="c">Евгений</div>
+
+				<!-- <div class="line"></div> -->
+
+				<div class="b">Возраст</div>
+				<div class="c">23</div>
+
+				<!-- <div class="line"></div> -->
+
+				<div class="b">Ищу</div>
+				<div class="c">Евгений</div>
+
+				<!-- <div class="line"></div> -->
+
+				<div class="b">О себе</div>
+				<div class="c">Евгений</div>
+			</div>
+
+
+		</div>
+	</div>
+	<footer>
+		<div class="line"></div>
+		<div class="footContain">
+			<div class="footNav">
+				<div class="fn"><a href="">О проекте</a></div>
+				<div class="fn"><a href="">Поддержка</a></div>
+				<div class="fn"><a href="">Блог</a></div>
+				<div class="fn"><a href="">Реклама</a></div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
